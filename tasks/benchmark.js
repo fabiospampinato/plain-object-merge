7513fe0dc8546852a3a5729f63d6666c1626754e
Fabio Spampinato <spampinabio@gmail.com>
2023-02-16 02:56:25 +0000

    Updated dependencies

--- tasks/benchmark.js
+++ tasks/benchmark.js
@@ -8,8 +8,7 @@ import Fixtures from '../test/fixtures.js';
 /* MAIN */
 
 benchmark.defaultOptions = Object.assign ( benchmark.defaultOptions, {
-  iterations: 100000,
-  log: 'compact'
+  iterations: 100_000
 });
 
 benchmark ({
